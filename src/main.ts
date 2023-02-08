95bae4f6b155e9989146efb2e994c0a317988668
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 11:53:09 -0400

    docs: add settings for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Notifications service')
+    .setDescription('notification microservice')
+    .setVersion('1.0')
+    .addTag('notification')
+    .setContact(
+      'Gabriel dos Santos Oliveira',
+      'https://www.linkedin.com/in/gabriel-dos-santos-oliveira-24b67b243/',
+      'gabrielsantosoliveira951@gmail.com',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
