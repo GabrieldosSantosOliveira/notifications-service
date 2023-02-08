81cb6ecb9fc5cfa74737aa4d0524b88c769d9417
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 11:59:47 -0400

    feat: remove controller and provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
+import { DatabaseModule } from '@infra/database/database.module';
+import { HttpModule } from '@infra/http/http.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { HttpModule } from './http.module';
-import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [HttpModule],
-  controllers: [AppController],
-  providers: [PrismaService],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
